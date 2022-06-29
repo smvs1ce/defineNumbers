99e220e7790721a70b40e77eafa83f7d32f6fee8
John Hairston <j.hairston71@gmail.com>
2022-06-29 10:38:56 -0400

    numbers defined

---
+++ src/numbers/numbers.java
@@ -0,0 +1,26 @@
+package numbers;
+
+public class numbers {
+
+	public static void main(String[] args) {
+		
+		//https://cs.fit.edu/~ryan/java/language/java-data.html
+	
+		int  a = 17;
+		
+		long b = 999999999L;
+		
+		float c = 3.2F;
+		
+		//double is a really big float
+		
+		double d = 3.22222222222222222222;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+	}
+
+}
